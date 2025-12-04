2403f32a8ad6ec1990a265c2ef9373fbe8f7232f
samman-khanal <samman.samman345@gmail.com>
2025-12-04 09:33:24 +0545

    Task-3

---
+++ Task-3/Program.cs
@@ -0,0 +1,25 @@
+﻿
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+        Console.WriteLine("Even Numbers:");
+        ProcessNumbers(numbers, num => num % 2 == 0);
+
+        Console.WriteLine("\nNumbers that are greater than 10 are:");
+        ProcessNumbers(numbers, num => num > 10);
+    }
+
+    public static void ProcessNumbers(int[] numbers, Func<int, bool> condition)
+    {
+        foreach (var num in numbers)
+        {
+            if (condition(num))
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
